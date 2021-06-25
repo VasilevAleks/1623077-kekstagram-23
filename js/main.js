a57a85f2798c726e9492108535988943b7ed482a
VasilevAleks <77631743+VasilevAleks@users.noreply.github.com>
2021-06-25 19:04:55 +0300

    Исправение №4

--- js/main.js
+++ js/main.js
@@ -1,8 +1,8 @@
-const MAX_COMMIT_LENGTH = 141;
+const MAX_COMMIT_LENGTH = 140;
 
-const GET_RANDOM_INTEGER = (min = 0, max = 0) => Math.floor(Math.random() * (max - min + 1)) + min;
+const getRandomInteger = (min = 0, max = 0) => Math.floor(Math.random() * (max - min + 1)) + min;
 
-const IS_VALID_LENGTH_COMMIT = (string = '') => string.length <= MAX_COMMIT_LENGTH;
+const isValidLengthCommit= (string = '') => string.length <= MAX_COMMIT_LENGTH;
 
-GET_RANDOM_INTEGER(1,100);
-IS_VALID_LENGTH_COMMIT('Hello, world');
+getRandomInteger(1,100);
+isValidLengthCommit('Hello, world');
