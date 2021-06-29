258678ec3d268111607120247f906d1165da0110
VasilevAleks <77631743+VasilevAleks@users.noreply.github.com>
2021-06-30 00:49:33 +0300

    исправление 4 раздела

--- js/main.js
+++ js/main.js
@@ -1,13 +1,6 @@
 const MAX_COMMIT_LENGTH = 140;
 
-const getRandomInteger = (min = 0, max = 0) => Math.floor(Math.random() * (max - min + 1)) + min;
-
-const isValidLengthCommit= (string = '') => string.length <= MAX_COMMIT_LENGTH;
-
-getRandomInteger(1,100);
-isValidLengthCommit('Hello, world');
-
-const MESSAGE = [
+const MESSAGES = [
   'Всё отлично!',
   'В целом всё неплохо. Но не всё.',
   'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.',
@@ -41,16 +34,60 @@ const NAMES = [
   'Ямато',
 ];
 
-const createPost = () => {
-  id: getRandomInteger(1,25),
-  url: 'photos/'+ getRandomInteger(1,25) +'.jpg',
-  description: 'Как я хорошо отдохнул в Египте',
-  likes: getRandomInteger(15,200),
-  comments: {
-    id: getRandomInteger(1,25),
-    avatar: 'img/avatar-'+getRandomInteger(1,6)+'.svg',
-    message: MESSAGE[getRandomInteger(0,MESSAGE.length-1)],
-    name: NAMES[getRandomInteger(0,NAMES.length-1)],
-  },
+const MIN_ID = 1;
+
+const MAX_ID = 25;
+
+const MAX_AVATAR = 6;
+
+const MIN_AVATAR = 7;
+
+const MIN_LIKE = 15;
+
+const MAX_LIKE = 200;
+
+const MAX_POSTS = 25;
+
+const PHOTOS = 'photos/';
+
+const JPG_FORMAT = '.jpg';
+
+const AVATARS = 'img/avatar-';
+
+const SVG_FORMAT = '.svg';
+
+const getRandomInteger = (min = 0, max = 0) => Math.floor(Math.random() * (max - min + 1)) + min;
+
+const isValidLengthCommit = (string = '') => string.length <= MAX_COMMIT_LENGTH;
+
+getRandomInteger(1,100);
+isValidLengthCommit('Hello, world');
+
+const postMessage = [];
+const addComments = () => {
+  const comments = [];
+
+  for (let i = 0; i <= MAX_COMMENTS; i++) {
+    comments.push({
+      id: getRandomInteger(MIN_ID,MAX_ID),
+      avatar:[AVATARS,getRandomInteger(MIN_AVATAR,MAX_AVATAR),SVG_FORMAT],
+      message: MESSAGE[getRandomInteger(0,MESSAGE.length-1)],
+      name: NAMES[getRandomInteger(0,NAMES.length-1)],
+    })
+  };
 };
+
+const createPost = (index) => {
+  id: getRandomInteger(MIN_ID,MAX_ID),
+  url: [PHOTOS,index,JPG_FORMAT],
+  description: 'Как я хорошо отдохнул в Египте',
+  likes: getRandomInteger(MIN_LIKE,MAX_LIKE),
+  comments: addComments,
+};
+
+const createPost = () => {
+  for (let i = 0; i <= MAX_POSTS; i++){
+    postMessage.push(createPost(i));
+  }
+}
 
